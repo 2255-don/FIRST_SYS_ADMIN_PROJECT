c3f146769f49ef7a15b8a054155a139f71e0a0da
2255-don <traorevetio22@gmail.com>
2025-09-02 03:34:42 +0000

    first commit

---
+++ nodeapp/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req,res)=>{
+    res.send("Server is up and running");
+});
+
+
+app.listen(8081, () => {
+    console.log("server is up");
+});
